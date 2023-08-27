7488e8f7618c0f8887c814aefd6277cccc72e94a
modip <mordip06@mail.com>
2023-08-27 01:09:05 +0000

    first commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 244, 242, 242),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "Galsen Travel",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: ClipOval(child: Image.asset('assets/venus.png')),
+            onPressed: () {},
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                width: 250,
+                height: 40,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey),
+                    boxShadow: [
+                      BoxShadow(
+                          offset: const Offset(3, 3),
+                          blurRadius: 10,
+                          color: Colors.white.withOpacity(0.15),
+                          spreadRadius: -2)
+                    ]),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search your ...",
+                    hintStyle: TextStyle(
+                      fontSize: 15,
+                      color: Colors.black,
+                    ),
+                    suffixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            SizedBox(
+              height: 20,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return const Padding(
+                      padding: EdgeInsets.only(right: 18, left: 8),
+                      child: Text(
+                        "hello",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    );
+                  }),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 12),
+                      child: Container(
+                        width: 150,
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Column(
+                          children: [
+                            Column(
+                              children: [Image.asset("assets/neptune.png")],
+                            ),
+                            const Spacer(),
+                            const Padding(
+                              padding: EdgeInsets.only(bottom: 12.0),
+                              child: SizedBox(
+                                child: Text(
+                                  "test,test,test,test",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            SizedBox(
+              height: 20,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Recommended,",
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 6,
+            ),
+            SizedBox(
+              height: 90,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 10,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(left: 8, right: 12),
+                      child: Container(
+                        width: 170,
+                        decoration: BoxDecoration(
+                            color: Colors.blueGrey,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Row(
+                          
+                          children: [
+                            SizedBox(
+                              child: Image.asset("assets/venus.png"),
+                            ),
+                            const SizedBox(
+                              child: Text(
+                                "test,test",
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        height: 42,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Icon(
+                Icons.home,
+                color: Colors.grey,
+              ),
+              Icon(
+                Icons.share,
+                color: Colors.grey,
+              ),
+              Icon(
+                Icons.add_a_photo,
+                color: Colors.grey,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
